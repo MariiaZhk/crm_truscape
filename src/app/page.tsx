5ed855d3fa9a16d94dc04b326dda4fd9edb19597
Mariia <maria8zhuk@gmail.com>
2024-09-24 00:40:17 +0200

    Implemented button, input-field, logo-uploader, new-company-form components. Api.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import StatusLabel, { Status } from './components/status-label';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl"> HomePage</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
