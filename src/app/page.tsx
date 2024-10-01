5b84d5ba81eab47a6a0dfeceb2abc74af7631721
Mariia <maria8zhuk@gmail.com>
2024-10-01 23:50:45 +0200

    Implemented error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AddNewCompanyButton from './components/add-new-company-button';
+import MagicButton from './components/magic-button';
 
 export default function HomePage() {
   return (
     <main>
       <h1 className="text-xl"> Home Page</h1>
       <AddNewCompanyButton />
+      <MagicButton />
     </main>
   );
 }
