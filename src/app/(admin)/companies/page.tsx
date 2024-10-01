13e2a9e4a4e578df2d55257d6441a4e57c309803
Mariia <maria8zhuk@gmail.com>
2024-10-01 14:20:30 +0200

    Implemented layout, sidebar, sidebar-item

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from '../components/header';
-import Toolbar from '../components/toolbar';
-import AddNewCompanyButton from '../components/add-new-company-button';
-import SearchInput from '../components/search-input';
-import CompanyTable from '../components/company-table';
-import CompanyRow from '../components/company-row';
+import Header from '../../components/header';
+import Toolbar from '../../components/toolbar';
+import AddNewCompanyButton from '../../components/add-new-company-button';
+import SearchInput from '../../components/search-input';
+import CompanyTable from '../../components/company-table';
+import CompanyRow from '../../components/company-row';
 import { Status } from '@/app/components/status-label';
 
 export interface PageProps {}
