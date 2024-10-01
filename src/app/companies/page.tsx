3bf6349ffe661262b9da30454576da303a794649
Mariia <maria8zhuk@gmail.com>
2024-10-01 11:41:45 +0200

    Implemented company-row, company-table, header, search-input, toolbar components

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../components/header';
+import Toolbar from '../components/toolbar';
+import AddNewCompanyButton from '../components/add-new-company-button';
+import SearchInput from '../components/search-input';
+import CompanyTable from '../components/company-table';
+import CompanyRow from '../components/company-row';
+import { Status } from '@/app/components/status-label';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <>
+      <Header>Companies</Header>
+      <Toolbar action={<AddNewCompanyButton />}>
+        <SearchInput />
+      </Toolbar>
+      <CompanyTable>
+        <CompanyRow
+          id={1}
+          category={'Products'}
+          company={'Costco'}
+          status={Status.Pending}
+          promotion={true}
+          country={'USA'}
+          joinedDate={'02.02.2020'}
+        ></CompanyRow>
+      </CompanyTable>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import AddNewCompanyButton from './components/add-new-company-button';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
-      <h1 className="text-xl"> HomePage</h1>
+      <h1 className="text-xl"> Home Page</h1>
       <AddNewCompanyButton />
     </main>
   );
