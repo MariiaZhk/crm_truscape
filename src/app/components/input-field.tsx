8675fe7088e664c4ecbe04603e40519cc837bfab
Mariia <maria8zhuk@gmail.com>
2024-10-02 22:57:46 +0200

    fixed types, fixed header

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { Field, FieldAttributes } from 'formik';
+
 import React from 'react';
+import { Field, FieldAttributes } from 'formik';
 
 export interface InputFieldProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
@@ -19,8 +20,8 @@ export default function InputField({ label, id, ...rest }: InputFieldProps) {
       <Field
         {...rest}
         id={id}
-        className="p-3 h-11 text-sm rounded border-gray-300 shadow"
-      ></Field>
+        className="p-3 h-11 text-sm rounded border border-gray-300 shadow"
+      />
     </div>
   );
 }
